a72c1ce2d4c328c672d634308b1fe066e5aeb668
TerryMac <TM@TerryMacs-MacBook-Pro.local>
2017-04-29 22:50:48 -0400

    update

--- Q200_Number_of_Islands.java
+++ Q200_Number_of_Islands.java
@@ -29,7 +29,30 @@
 // I can use bitwise operator for this problem.
 
 public class Solution {
-    public int numIslands(char[][] grid) {
 
+private int n;
+private int m;
+
+public int numIslands(char[][] grid) {
+    int count = 0;
+    n = grid.length; // n = length of column
+    if (n == 0) return 0;
+    m = grid[0].length; // m = length of row
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++)
+            if (grid[i][j] == '1') {
+                DFSMarking(grid, i, j);
+                ++count;
+            }
     }
+    return count;
+}
+
+private void DFSMarking(char[][] grid, int i, int j) {
+    if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
+    grid[i][j] = '0';
+    DFSMarking(grid, i + 1, j);
+    DFSMarking(grid, i - 1, j);
+    DFSMarking(grid, i, j + 1);
+    DFSMarking(grid, i, j - 1);
 }
