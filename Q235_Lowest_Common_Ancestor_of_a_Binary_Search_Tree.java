c582a62d7f52640209ddb91db46cdd8ec17fbc72
TerryMac <newforce007@gmail.com>
2017-05-17 12:25:37 -0400

    LCA_BST

--- Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
+++ Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -1,28 +1,36 @@
 235. Lowest Common Ancestor of a Binary Search Tree
 
-Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two
+given nodes in the BST.
 
-According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is
+defined between two nodes v and w as the lowest node in T that has both v and w
+as descendants (where we allow a node to be a descendant of itself).”
 
 
-_______6______
- /              \
-___2__          ___8__
+     _______6______
+    /              \
+ ___2__          ___8__
 /      \        /      \
 0      _4       7       9
-   /  \
-   3   5
+      /  \
+      3   5
 
 
-For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another
+example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself
+according to the LCA definition.
 
 
 
 
 
 
-3 lines with O(1) space, 1-Liners, Alternatives
-Just walk down from the whole tree's root as long as both p and q are in the same subtree (meaning their values are both smaller or both larger than root's). This walks straight from the root to the LCA, not looking at the rest of the tree, so it's pretty much as fast as it gets. A few ways to do it:
+3 lines with O(1) space, 1-Liners, Alternatives Just walk down from the whole
+tree's root as long as both p and q are in the same subtree (meaning their
+values are both smaller or both larger than root's). This walks straight from
+the root to the LCA, not looking at the rest of the tree, so it's pretty much as
+fast as it gets. A few ways to do it:
 
 Iterative, O(1) space
 
@@ -32,6 +40,8 @@ def lowestCommonAncestor(self, root, p, q):
     while (root.val - p.val) * (root.val - q.val) > 0:
         root = (root.left, root.right)[p.val > root.val]
     return root
+
+
 Java
 
 public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
@@ -39,7 +49,11 @@ public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         root = p.val < root.val ? root.left : root.right;
     return root;
 }
-(in case of overflow, I'd do (root.val - (long)p.val) * (root.val - (long)q.val))
+
+(in case of overflow, I'd do (root.val - (long)p.val) * (root.val -
+(long)q.val))
+
+
 
 Different Python
 
@@ -60,6 +74,7 @@ def lowestCommonAncestor(self, root, p, q):
             return root
 Recursive
 
+
 Python
 
 def lowestCommonAncestor(self, root, p, q):
@@ -71,12 +86,16 @@ Python One-Liner
 def lowestCommonAncestor(self, root, p, q):
     return root if (root.val - p.val) * (root.val - q.val) < 1 else \
            self.lowestCommonAncestor((root.left, root.right)[p.val > root.val], p, q)
+
+
 Java One-Liner
 
 public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
     return (root.val - p.val) * (root.val - q.val) < 1 ? root :
            lowestCommonAncestor(p.val < root.val ? root.left : root.right, p, q);
 }
+
+
 "Long" Python, maybe easiest to understand
 
 def lowestCommonAncestor(self, root, p, q):
@@ -85,6 +104,3 @@ def lowestCommonAncestor(self, root, p, q):
     if p.val > root.val < q.val:
         return self.lowestCommonAncestor(root.right, p, q)
     return root
-
-
-    
